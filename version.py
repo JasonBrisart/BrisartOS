dce83f9c6fc97529fa1c33f36a0b75c93743f580
Jason Brisart <jason@brisartresearcharchive.com>
2026-08-29 14:32:30 -0400

    Update version to 0.10.0-alpha

--- version.py
+++ version.py
@@ -4,7 +4,7 @@ Single source of truth for the BrisartOS version string.
 Pure Python. No dependencies.
 """
 NAME = "BrisartOS"
-VERSION = "0.9.1-alpha"
+VERSION = "0.10.0-alpha"
 
 
 def version_text():
